548d6d7a7ae3e15cf9e81b86142faa89bf5e458b
Yoon <hyooni6323@gmail.com>
2025-02-28 16:47:35 +0900

    Add: backend api - useAppAPI

---
+++ src/apis/useAppApi.js
@@ -0,0 +1,49 @@
+import { useMemo } from "react";
+
+const appProxyTarget = import.meta.env.VITE_APP_PROXY
+  ? `http://${import.meta.env.VITE_APP_PROXY}/api`
+  : "http://localhost:8000/api"; // 기본값 설정
+
+// fast-api 백엔드 API
+export const useAppApi = () => {
+  const prefix = "/api"; // fast api 공통 prefix
+
+  const apiMethods = useMemo(
+    () => ({
+      getCameraSettings: async () => {
+        try {
+          const response = await fetch(`${appProxyTarget}/config/cameras`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return await response.json();
+        } catch (error) {
+          throw error;
+        }
+      },
+      getROIImage: async (streamId) => {
+        try {
+          const response = await fetch(
+            `${appProxyTarget}/config/roi?camera_id=${streamId}`
+          );
+          if (!response.ok)
+            throw new Error(`HTTP error! Status: ${response.status}`);
+
+          const blob = await response.blob();
+          return URL.createObjectURL(blob); // 이미지 URL 반환
+        } catch (error) {
+          console.error("Failed to load ROI image:", error);
+          return null;
+        }
+      },
+    }),
+    []
+  );
+
+  return apiMethods;
+};
