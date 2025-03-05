cfae7ff8324052c19f5e3a9a53c1501e6dcdfec8
Yoon <hyooni6323@gmail.com>
2025-03-05 16:10:06 +0900

    Update: Configuration 기본 컴포넌트 업데이트

--- src/components/CameraSettingsTable.jsx
+++ src/components/CameraSettingsTable.jsx
@@ -1,43 +1,19 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import CustomDataItem from "./CustomDataItem";
+import { useTranslation } from "@ultivis/library";
 
-const CameraSettingsTable = ({ name, data }) => {
-  console.log("CameraSettingsTable - Received Data:", data); // 확인용
-
-  // 데이터 리스트를 상태로 관리
-  const [cameraList, setCameraList] = useState(
-    Object.entries(data || {}).map(([key, value]) => ({
-      id: key,
-      ...(value ?? {
-        label: "",
-        protocol: "",
-        url: "",
-        roi: { enable: false, type: "", points: [] },
-      }),
-    }))
-  );
-
-  // 특정 카메라 데이터를 업데이트하는 함수
-  const updateCameraData = useCallback((index, updatedData) => {
-    setCameraList((prev) =>
-      prev.map((item, i) => (i === index ? { ...item, ...updatedData } : item))
-    );
-    console.log("업데이트된 cameraList:", cameraList);
-  }, []);
-
+const CameraSettingsTable = ({ data }) => {
+  const { t } = useTranslation();
   return (
     <div className="border rounded-lg p-2 shadow-md">
-      {cameraList.length === 0 ? (
-        <p className="text-center text-gray-500">No cameras available.</p>
-      ) : (
-        cameraList.map((camera, index) => (
-          <CustomDataItem
-            key={camera.id}
-            data={camera}
-            index={index}
-            onUpdate={updateCameraData}
-          />
+      {data && Object.entries(data).length > 0 ? (
+        Object.entries(data).map(([cameraId, cameraData]) => (
+          <CustomDataItem key={cameraId} id={cameraId} data={cameraData} />
         ))
+      ) : (
+        <p className="text-center text-gray-500">
+          {t(`No cameras available.`)}
+        </p>
       )}
     </div>
   );
--- src/components/CustomDataItem.jsx
+++ src/components/CustomDataItem.jsx
@@ -5,7 +5,6 @@ import {
   Button,
   Switch,
   ArrowDownIcon,
-  useDatapointActions,
   useTranslation,
   Select,
   SelectTrigger,
@@ -13,182 +12,279 @@ import {
   SelectContent,
   SelectItem,
   SelectGroup,
+  Form,
+  useToast,
 } from "@ultivis/library";
+import { useAppApi } from "../apis/useAppApi";
 import CustomDialog from "./CustomDialog";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import VideoModal from "./VideoModal";
+import { useConfigurationSchema } from "./schema/useConfigurationSchema";
 
-const CustomDataItem = ({ data, index, onUpdate }) => {
+const protocolOptions = [
+  { name: "HLS", value: "hls" },
+  { name: "RTSP", value: "rtsp" },
+  { name: "None", value: "none" },
+];
+
+const CustomDataItem = ({ id, data }) => {
   const { t } = useTranslation();
-  const [currentData, setCurrentData] = useState(data); // 개별 상태 관리
+  const { updateCamera } = useAppApi();
+
+  const configSchema = useConfigurationSchema();
+  const { toast } = useToast();
+
   const [isCollapse, setIsCollapse] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isVideoOpen, setIsVideoOpen] = useState(false);
 
-  // 취소 버튼을 누르면 원래 값으로 복원
-  const handleCancel = () => {
-    setCurrentData(data); // 초기 데이터로 복원
+  //  데이터가 null일 경우 최초 입력 시 기본값 적용
+  const defaultData = data ?? {
+    label: "",
+    protocol: "",
+    url: "",
+    active: false,
+    roi: { enable: false, type: "", points: [] },
   };
 
-  // 저장 버튼을 누르면 부모 컴포넌트에 변경 내용 전달
-  const handleSave = () => {
-    onUpdate(index, currentData);
+  //  react-hook-form 적용
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    trigger,
+    watch,
+    reset,
+    clearErrors,
+    setError,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(configSchema),
+    defaultValues: defaultData,
+    mode: "onBlur",
+  });
+
+  const protocol = watch("protocol");
+  const url = watch("url");
+  const roi = watch("roi"); // Watch roi object
+
+  const onSubmit = async (formData) => {
+    if (!formData) {
+      toast({
+        title: t(`Error! No data to save.`),
+        duration: 3000,
+        variant: "destructive",
+      });
+    }
+    try {
+      const response = await updateCamera(id, formData); // Use updateCamera function to submit data
+      toast({
+        title: t(`Settings updated successfully!`),
+        duration: 3000,
+        variant: "success",
+      });
+      setIsCollapse(false);
+    } catch (error) {
+      toast({
+        title: t(`Error updating settings`),
+        duration: 3000,
+        variant: "destructive",
+      });
+      console.error("Error updating settings:", error);
+    }
   };
 
   // 필드 변경 시 상태 업데이트
-  const onChangeField = (key, value) => {
-    setCurrentData((prev) => ({
-      ...prev,
-      [key]: value,
-    }));
-
-    console.log(key, value);
+  // ROI 값 업데이트 방식 수정
+  const updateROIField = (updatedROI) => {
+    setValue("roi", updatedROI); // ⬅ "roi" 키 없이 전체 객체 업데이트
   };
-
-  console.log(currentData); // 변경된 데이터를 확인하기 위한 console.log
-
-  // protocol 옵션
-  const displayOptions = [
-    { name: "HLS", value: "hls" },
-    { name: "RTSP", value: "rtsp" },
-    { name: "None", value: "none" },
-  ];
-
   return (
-    <>
-      <div className="mb-2 box-border flex items-center justify-between rounded-lg border border-grayscale-600 px-4 py-2.5 dark:border-dark-grayscale-400">
-        <div className="flex h-9 w-full items-center">
-          <Switch
-            checked={currentData.__active === true}
-            onCheckedChange={(checked) => {
-              onChangeField("__active", checked);
-            }}
-          />
+    <Form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-2 box-border flex items-center justify-between rounded-lg border border-grayscale-600 px-4 py-2.5 dark:border-dark-grayscale-400">
+          <div className="flex h-9 w-full items-center">
+            <Switch
+              {...register("active")}
+              checked={watch("active")}
+              onCheckedChange={(checked) => setValue("active", checked)}
+            />
 
-          <p className="w-full mx-2 overflow-hidden text-ellipsis whitespace-nowrap text-base font-bold text-grayscale-100 dark:text-dark-grayscale-100">
-            {currentData.id}
-          </p>
-          <Button
-            title={t("expand")}
-            size="icon"
-            type="button"
-            variant="ghost"
-            className={`mx-1 h-9 text-grayscale-300 transition duration-300 ease-in-out ${
-              isCollapse ? "rotate-180" : "rotate-0"
-            }`}
-            onClick={() => setIsCollapse(!isCollapse)}
-          >
-            <ArrowDownIcon className="dark:text-dark-grayscale-200" />
-          </Button>
+            <p className="w-full mx-2 overflow-hidden text-ellipsis whitespace-nowrap text-base font-bold text-grayscale-100 dark:text-dark-grayscale-100">
+              {id}
+            </p>
+            <Button
+              title={t("expand")}
+              size="icon"
+              type="button"
+              variant="ghost"
+              className={`mx-1 h-9 text-grayscale-300 transition duration-300 ease-in-out ${
+                isCollapse ? "rotate-180" : "rotate-0"
+              }`}
+              onClick={() => setIsCollapse(!isCollapse)}
+            >
+              <ArrowDownIcon className="dark:text-dark-grayscale-200" />
+            </Button>
+          </div>
         </div>
-      </div>
-      {/* ---------------------------- DataDetail ---------------------------- */}
-      {isCollapse && (
-        <>
-          <div className="mx-2 mt-[5px] bg-[#fbfbfc] text-xs dark:bg-dark-bg-333">
-            <fieldset className="mb-4 border p-4 text-sm dark:border-dark-grayscale-500">
-              <div className="mb-4">
-                <Label className="mb-2 flex max-w-fit items-center font-semibold leading-5">
-                  {t("label")}
-                </Label>
-                <Input
-                  className="w-full p-2"
-                  type="text"
-                  value={currentData.label ?? ""} // currentData를 참조
-                  onChange={(e) => onChangeField("label", e.target.value)} // 값 변경 시 currentData 업데이트
-                />
-              </div>
-              <div className="flex items-center justify-between gap-x-4">
-                <div
-                  className="flex flex-col justify-between"
-                  style={{ flex: 2 }}
-                >
-                  <Label className="mb-2 flex max-w-fit items-center font-semibold leading-2">
-                    {t("protocol")}
-                  </Label>
-                  <Select
-                    value={currentData.protocol || "none"} // currentData를 참조
-                    onValueChange={(value) =>
-                      onChangeField("protocol", value === "none" ? "" : value)
-                    } // 값 변경 시 currentData 업데이트
-                  >
-                    <SelectTrigger>
-                      <SelectValue placeholder={t("Select Protocol")} />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        {displayOptions.map(({ name, value }) => (
-                          <SelectItem key={value} value={value}>
-                            {t(name)}
-                          </SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div
-                  className="flex flex-col justify-between"
-                  style={{ flex: 7 }}
-                >
+        {/* ---------------------------- DataDetail ---------------------------- */}
+        {isCollapse && (
+          <>
+            <div className="mx-2 mt-[5px] bg-[#fbfbfc] text-xs dark:bg-dark-bg-333">
+              <fieldset className="mb-4 border p-4 text-sm dark:border-dark-grayscale-500">
+                <div className="mb-4">
                   <Label className="mb-2 flex max-w-fit items-center font-semibold leading-5">
-                    {t("url")}
+                    {t("label")}
                   </Label>
-                  <Input
-                    type="text"
-                    value={currentData.url ?? ""} // currentData를 참조
-                    placeholder={t("")} // TODO: protocol에 따라 placeholder 변경 가능
-                    onChange={(e) => onChangeField("url", e.target.value)} // 값 변경 시 currentData 업데이트
-                  />
+                  <Input {...register("label")} className="w-full p-2" />
+                </div>
+                <div className="flex items-start justify-between gap-x-4">
+                  {/* Protocol 필드 */}
+                  <div
+                    className="flex flex-col justify-between h-[80px]"
+                    style={{ flex: 2 }}
+                  >
+                    <Label className="mb-2 flex max-w-fit items-center font-semibold leading-2">
+                      {t("protocol")}
+                    </Label>
+                    <Select
+                      value={protocol || "none"}
+                      onValueChange={(value) => {
+                        setValue("protocol", value === "none" ? "" : value);
+                        if (value === "none") {
+                          setValue("url", ""); // ⬅ protocol이 "none"이면 url도 ""로 초기화
+                          clearErrors("url");
+                        }
+                      }}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder={t("Select Protocol")} />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          {protocolOptions.map(({ name, value }) => (
+                            <SelectItem key={value} value={value}>
+                              {t(name)}
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  {/* URL 필드 */}
+                  <div
+                    className="flex flex-col justify-between h-[80px]"
+                    style={{ flex: 7 }}
+                  >
+                    <Label className="mb-2 flex max-w-fit items-center font-semibold leading-5">
+                      {t("url")}
+                    </Label>
+                    <Input
+                      {...register("url")}
+                      disabled={protocol === ""}
+                      className="w-full p-2"
+                    />
+                    <p className="text-red-500 text-sm min-h-[20px]">
+                      {errors.url?.message || " "}
+                    </p>{" "}
+                    {/* 에러가 없을 때도 공간을 차지하게 만듦 */}
+                  </div>
+
+                  {/* Test 버튼 */}
+                  <div className="flex flex-col justify-between h-[80px] mt-9">
+                    <Button
+                      type="button"
+                      disabled={protocol === "" || url === ""}
+                      onClick={() => setIsVideoOpen(true)}
+                    >
+                      {t("check")}
+                    </Button>
+                    <VideoModal
+                      id={id}
+                      isOpen={isVideoOpen}
+                      onClose={() => setIsVideoOpen(false)}
+                      protocol={protocol}
+                      url={url}
+                    />
+                  </div>
                 </div>
 
-                <div className="flex flex-col justify-between mt-6">
-                  <Button type="button" onClick={() => setIsDialogOpen(true)}>
-                    {t("test")}
-                  </Button>
-                </div>
-              </div>
+                <fieldset
+                  className="mt-1 border text-sm dark:border-dark-grayscale-500"
+                  style={{ padding: "1rem 1rem 0.5rem 1rem" }}
+                >
+                  <legend className="text-gray-500 ">{t("ROI")}</legend>
+                  <div className="flex h-9 w-full items-center">
+                    <Switch
+                      {...register("roi.enable")}
+                      checked={watch("roi.enable")}
+                      onCheckedChange={async (checked) => {
+                        if (checked) {
+                          if (
+                            !roi?.type ||
+                            (roi?.points && roi?.points.length === 0)
+                          ) {
+                            await trigger("roi");
+                            setError("roi", {
+                              type: "manual",
+                              message: t(`ROI type and area must be specified`),
+                            });
 
-              <fieldset className="mt-4 border p-4 text-sm dark:border-dark-grayscale-500">
-                <legend className="text-gray-500 ">{t("ROI")}</legend>
+                            setTimeout(() => {
+                              clearErrors("roi");
+                            }, 3000);
 
-                <div className="flex h-9 w-full items-center">
-                  <Switch
-                    checked={currentData.roi.enable === true}
-                    onCheckedChange={(checked) => {
-                      onChangeField("roi", {
-                        ...currentData.roi,
-                        enable: checked,
-                      });
-                    }}
-                  />
+                            return;
+                          }
+                        }
+                        setValue("roi.enable", checked);
+                      }}
+                    />
 
-                  <p className="w-full mx-2 overflow-hidden text-ellipsis whitespace-nowrap text-base font-bold text-grayscale-100 dark:text-dark-grayscale-100">
-                    {t("roi")}
+                    <p className="w-full mx-2 overflow-hidden text-ellipsis whitespace-nowrap text-base font-bold text-grayscale-100 dark:text-dark-grayscale-100">
+                      {t("roi")}
+                    </p>
+                    <Button
+                      type="button"
+                      onClick={() => setIsDialogOpen(true)}
+                      disabled={protocol === "" || url === ""}
+                    >
+                      {t("configuration")}
+                    </Button>
+
+                    <CustomDialog
+                      id={id}
+                      isOpen={isDialogOpen}
+                      onClose={() => setIsDialogOpen(false)}
+                      data={watch("roi")}
+                      onUpdate={updateROIField}
+                    />
+                  </div>
+                  <p className="text-red-500 text-sm min-h-[20px]">
+                    {errors.roi?.message || " "}
                   </p>
-                  <Button type="button" onClick={() => setIsDialogOpen(true)}>
-                    {t("configuration")}
-                  </Button>
+                </fieldset>
 
-                  <CustomDialog
-                    id={currentData.id}
-                    isOpen={isDialogOpen}
-                    onClose={() => setIsDialogOpen(false)}
-                    data={currentData.roi} // roi 데이터를 전달
-                    onUpdate={onChangeField} // ROI 업데이트 처리
-                  />
+                <div className="flex flex-row justify-end space-x-4 pt-4">
+                  <Button
+                    variant="destructive"
+                    onClick={() => reset(defaultData)}
+                  >
+                    {t("cancel")}
+                  </Button>
+                  <Button variant="destructive" type="submit">
+                    {t("save")}
+                  </Button>
                 </div>
               </fieldset>
-              <div className="flex flex-row justify-end space-x-4 pt-6">
-                <Button variant="destructive" onClick={handleCancel}>
-                  {t("cancel")}
-                </Button>
-                <Button variant="destructive" onClick={handleSave}>
-                  {t("save")}
-                </Button>
-              </div>
-            </fieldset>
-          </div>
-          {/* DataItem */}
-        </>
-      )}
-    </>
+            </div>
+            {/* DataItem */}
+          </>
+        )}
+      </form>
+    </Form>
   );
 };
 
